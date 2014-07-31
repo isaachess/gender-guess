f1ffe29e8d6bca3d941bfdd549e3d4fe69ebc37a
Isaac Hess <isaachess@gmail.com>
2014-07-31 15:51:24 -0600

    Working

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var fs = require('fs')
 
 gulp.task('concat-names', function() {
-    gulp.src('./lib/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist/'))
-});
+    gulp.src(['./names/*.txt'])
+        .pipe(concat('combined.csv'))
+        .pipe(gulp.dest('./names/'))
+})
