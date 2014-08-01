393b92e19c21471e6c70022cdb03a13ac2a8f438
Isaac Hess <isaachess@gmail.com>
2014-07-31 20:24:29 -0600

    Almost done with skeleton npm package

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var csv2json = require('gulp-csv2json')
+var rename = require('gulp-rename')
 var fs = require('fs')
 
 gulp.task('concat-names', function() {
     gulp.src(['./names/*.txt'])
         .pipe(concat('combined.csv'))
+        .pipe(gulp.dest('./names/'))
+})
+
+gulp.task('toJson', function() {
+    gulp.src('names/combined1.csv')
+        .pipe(csv2json())
+        .pipe(rename({extname: '.json'}))
         .pipe(gulp.dest('./names/'))
 })
